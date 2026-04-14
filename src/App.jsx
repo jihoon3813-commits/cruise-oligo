03905bc0acf7b0375770cf2820cd0018036b109b
jihoon3813-commits <jihoon3813@github.com>
2026-04-14 13:18:52 +0900

    UX: Implemented ScrollToTop to reset scroll position on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import ProductDetail from './pages/ProductDetail';
 import Reviews from './pages/Reviews';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
